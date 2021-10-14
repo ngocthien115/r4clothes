6840317544286e1cb29685b71ec9152f1a13b3ed
ngocthien115 <nthien390@gmail.com>
2021-10-14 16:08:13 +0700

    init and create models and services

---
+++ R4Clothes.Shared/Helpers/MaHoaHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R4Clothes.Shared.Helpers
+{
+    interface IMaHoaHelper
+    {
+        string Mahoa(string source);
+    }
+    public class MaHoaHelper : IMaHoaHelper
+    {
+        public string Mahoa(string source)
+        {
+            string hash = "";
+            using (var md5Hash = MD5.Create())
+            {
+                var sourceBytes = Encoding.UTF8.GetBytes(source);
+                var hashBytes = md5Hash.ComputeHash(sourceBytes);
+                hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+            }
+            return hash;
+        }
+    }
+}
