a4f521c6b73688b06de6f94d295686643108915d
nguyenminhnhat150101 <nguyenminhnhat15012001@gmail.com>
2021-11-02 19:08:46 +0700

    AddServices + ViewModels + ViewLogin

---
+++ R4Clothes.Shared/Services/ChiaSeSvc.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using R4Clothes.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R4Clothes.Shared.Services
+{
+    public interface IChiSe
+    {
+        bool AddChiSe(ChiaSe chiaSe);
+    }
+    public class ChiaSeSvc : IChiSe
+    {
+        public DataContext _context;
+        public ChiaSeSvc(DataContext context)
+        {
+            _context = context;
+        }
+        public bool AddChiSe(ChiaSe chiaSe)
+        {
+            bool ret;
+            try
+            {
+                //  NetworkCredential cred = new NetworkCredential("driverhuyhoa@gmail.com","01635592943");
+                MailMessage Msg = new MailMessage();               
+                //sender email address
+                Msg.From = new MailAddress("phamhuyhoalk20@gmail.com");
+                //Recipient e-mail address
+                Msg.To.Add(chiaSe.Email);
+                //Assign the subject  of out message
+                Msg.Subject = "Bạn đã sử dụng tính năng chia sẻ";
+                Msg.Body = "Chào " + chiaSe.Hoten + "! " + chiaSe.Makhachhang + " đã chia sẻ " + chiaSe.Masanpham + " đến bạn.";
+                Msg.Body = "" + chiaSe.Thoigian;
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtp.Credentials = new NetworkCredential("phamhuyhoalk20@gmail.com", "01635592943");
+                    smtp.EnableSsl = true;
+                    smtp.Send(Msg);
+                }
+                ret = true;
+            }
+            catch (Exception)
+            {
+                ret = false;
+            }
+            return ret;
+        }
+    }
+}
--- R4Clothes.Shared/Services/DanhGiaSanPhamSvc.cs
+++ R4Clothes.Shared/Services/DanhGiaSanPhamSvc.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using R4Clothes.Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +10,38 @@ namespace R4Clothes.Shared.Services
 {
     public interface IDanhGiaSanPham
     {
-        bool AddDanhGiaSanPham(int idsanpham, string danhgia);
-        string XemDanhGiaSanPham(int idsanpham);
+        bool AddDanhGiaSanPham(DanhGiaSanPham danhGiaSanPham);
+        List<DanhGiaSanPham> XemDanhGiaTheoSanPham(int idsanpham);
     }
     public class DanhGiaSanPhamSvc : IDanhGiaSanPham
     {
-        public bool AddDanhGiaSanPham(int idsanpham, string danhgia)
+        public DataContext _context;
+        public DanhGiaSanPhamSvc(DataContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
-
-        public string XemDanhGiaSanPham(int idsanpham)
+        public bool AddDanhGiaSanPham(DanhGiaSanPham danhGiaSanPham)
         {
-            throw new NotImplementedException();
+            bool ret;
+            try
+            {
+                _context.Add(danhGiaSanPham);
+                _context.SaveChanges();
+                ret = true;
+            }
+            catch
+            {
+                ret = false;
+            }
+            return ret;
+        }
+        public List<DanhGiaSanPham> XemDanhGiaTheoSanPham(int idsanpham)
+        {
+            List<DanhGiaSanPham> list = new List<DanhGiaSanPham>();
+            list = _context.DanhGiaSanPhams.Where(x => x.Masanpham == idsanpham).OrderByDescending(x => x.Thoigian)
+                .Include(x => x.SanPham)
+                .ToList();           
+            return list;
         }
     }
 }
